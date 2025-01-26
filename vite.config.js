7ca5066d69a407544717942b15501638e8f81800
Esmat <33832742+esmatshamsodin@users.noreply.github.com>
2025-01-26 20:24:37 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    historyApiFallback: true,  // این گزینه را بردارید
-    watch: {
-      usePolling: false, // بررسی تغییرات بدون استفاده از polling
-    },
-    hmr: {
-      overlay: false, // غیرفعال کردن نمایش خطاهای HMR در مرورگر
-    },
-  },
+  base:"/FinPro"
 })
